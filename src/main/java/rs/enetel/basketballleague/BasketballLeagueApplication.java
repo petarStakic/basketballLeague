2555f60ec68c2497ff07e6cd8ef8f194889f79ef
Petar Stakic <petar.stakic@enetelsolutions.com>
2018-02-08 12:13:35 +0100

    Initial, empty Spring Boot project

---
+++ src/main/java/rs/enetel/basketballleague/BasketballLeagueApplication.java
@@ -0,0 +1,14 @@
+package rs.enetel.basketballleague;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballLeagueApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(BasketballLeagueApplication.class, args);
+	}
+}
