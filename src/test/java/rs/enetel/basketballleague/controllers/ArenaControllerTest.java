8f5cf3614fd309d989aa07304122a1e34f1a2fba
Petar Stakic <petar.stakic@enetelsolutions.com>
2018-02-13 17:03:48 +0100

    Added a Bootstrap css to the arenas/all view template. Also created test classes for ArenaRepositoryImpl and ArenaController. But implemented only two tests just for ArenaController...

---
+++ src/test/java/rs/enetel/basketballleague/controllers/ArenaControllerTest.java
@@ -0,0 +1,82 @@
+package rs.enetel.basketballleague.controllers;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.eq;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.ui.Model;
+
+import rs.enetel.basketballleague.dao.Arena;
+import rs.enetel.basketballleague.repositories.ArenaRepository;
+
+public class ArenaControllerTest
+{
+
+	ArenaController arenaController;
+
+	@Mock
+	ArenaRepository arenaRepository;
+	
+	@Mock
+	Model model;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		MockitoAnnotations.initMocks(this);
+
+		arenaController = new ArenaController(arenaRepository);
+	}
+	
+	@Test
+	public void testMockMvc() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(arenaController).build();
+		
+		mockMvc.perform(get("/arenas"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("arenas/all"));
+	}
+	
+	@Test
+	public void showAllTest() throws Exception
+	{
+		// Given
+		String templatePath;
+		
+		List<Arena> arenas = new ArrayList();
+		arenas.add(new Arena());
+		arenas.add(new Arena());
+		
+		when(arenaRepository.all()).thenReturn(arenas);
+		
+		ArgumentCaptor<List<Arena>> argumentCaptor = ArgumentCaptor.forClass(List.class);
+		
+		// When
+		templatePath = arenaController.showAll(model);
+		
+		// Then
+		assertEquals("arenas/all", templatePath);
+		verify(arenaRepository, times(1)).all();
+		verify(model, times(1)).addAttribute(eq("arenas"), argumentCaptor.capture());
+		List<Arena> listInController = argumentCaptor.getValue();
+		assertEquals(2, listInController.size());
+	}
+}
---
+++ src/test/java/rs/enetel/basketballleague/repositories/ArenaRepositoryImplTest.java
@@ -0,0 +1,8 @@
+package rs.enetel.basketballleague.repositories;
+
+// TODO: pitanje: kako unit-testirati metode koje koriste JdbcTemplate da komuniciraju sa bazom 
+public class ArenaRepositoryImplTest
+{
+	ArenaRepositoryImpl arenaRepository;
+	
+}
